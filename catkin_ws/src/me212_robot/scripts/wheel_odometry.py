2dbd63bb4adc153e1815577c42f8f6f4a06dca50
Louis Gatin <l.gatin@neuf.fr>
2017-10-03 14:36:41 +0800

    Added path points

--- catkin_ws/src/me212_robot/scripts/wheel_odometry.py
+++ catkin_ws/src/me212_robot/scripts/wheel_odometry.py
@@ -47,10 +47,14 @@ class wheelOdometry(object):
                                       "base_link", # robot frame
                                       "map") # base frame
 	def publish_marker(self):
-		points = []	
-                #####################################################
-		# generate your trajectory
-                #####################################################
+		points = []
+	    for i in np.linspace(0, 1):             # generate points of a straight line (length = 1 m)
+	        points.append([i, 0, 0])
+	    for i in np.linspace(-np.pi/2, np.pi/2): # generate points of a semi-circle (r =0.25 m)
+	        points.append([np.cos(i)*0.25 + 1, np.sin(i)*0.25 + 0.25, 0])
+	    for i in np.linspace(0, 1):             # generate points of a straight line (length = 1 m)
+	        points.append([i, 0.5, 0])
+
 
 		for i in xrange(0,9):
 			self.vis_pub.publish(self.createPointMarker2(points, 1, [0.6, 0.6, 0, 1]))
@@ -66,7 +70,7 @@ class wheelOdometry(object):
 
 		n = len(points)
 		sub = 1
-	
+
 		if rgba is not None:
 			marker.color.r, marker.color.g, marker.color.b, marker.color.a = tuple(rgba)
 
